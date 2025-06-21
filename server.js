3e574700716020e47a4353dc7dd546270375c001
BYC3448 <qtbbo@naver.com>
2025-06-21 11:55:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/api/gemini/generate', async (req, res) => {
             ],
             generationConfig: {
                 temperature: 0.7,
-                maxOutputTokens: 4000
+                maxOutputTokens: 6000
             }
         };
 
